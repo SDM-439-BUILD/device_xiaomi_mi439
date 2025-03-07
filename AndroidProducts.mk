5cb5f968b0ea97da88bb62568b9ab99c001c839a
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-03-08 05:22:14 +0600

    Upgrade

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,14 @@
 #
 # Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2023 Paranoid Android
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_mi439.mk
+    $(LOCAL_DIR)/lineage_mi439.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_mi439-user \
-    aosp_mi439-userdebug \
-    aosp_mi439-eng
+    lineage_mi439-user \
+    lineage_mi439-userdebug \
+    lineage_mi439-eng
